47ad7e01f5d8cd5552c965f7dffd834d8b3b25e7
Hao Chen <hao.chen@avalara.com>
2015-12-25 13:23:40 -0800

    move zeroes

---
+++ src/helper.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python2.7
+
+'''
+helper function for common utilities
+'''
+
+__author__ = 'hao.chen'
+
+def swap(a, b):
+    a, b = b, a
+
+
+
+if __name__ == "__main__":
+    swap(0, 1)
---
+++ src/move_zeroes.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python2.7
+
+
+'''
+Given an array nums, write a function to move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+
+For example, given nums = [0, 1, 0, 3, 12], after calling your function, nums should be [1, 3, 12, 0, 0].
+
+Note:
+You must do this in-place without making a copy of the array.
+Minimize the total number of operations.
+'''
+
+__author__ = 'hao.chen'
+
+class Solution(object):
+    def move_zeroes(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: void Do not return anything, modify nums in-place instead.
+        """
+        if len(nums) == 0 or len(nums) == 1:
+            return
+        p1 = 0
+        p2 = 0
+        for num in nums:
+            if num == 0:
+                break
+            p1 += 1
+            p2 += 1
+
+        while p2 < len(nums):
+            if nums[p2] != 0:
+                nums[p1], nums[p2] = nums[p2], nums[p1]
+                p1 += 1
+            p2 += 1
+
+
+if __name__ == "__main__":
+    sol = Solution()
+    nums = [0, 1, 0, 3, 12]
+    sol.move_zeroes(nums)
+    print nums
