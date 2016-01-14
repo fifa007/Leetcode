2faafad599844884ded2be0d7a0f50a55dd7308e
Hao Chen <hao.chen@avalara.com>
2016-01-13 21:19:27 -0800

    implementing linked list

--- src/data_structure.py
+++ src/data_structure.py
@@ -14,13 +14,61 @@ class tree_node(object):
         self.right = None
 
 
-#definition for single-linked list
+#definition for list node
 class list_node(object):
-    def __init__(self, x):
+    def __init__(self, x = None, next = None):
         self.val = x
         self.next = None
 
     def __eq__(self, other):
         return self.val == other
+
+    def get_data(self):
+        return self.val
+
+    def get_next(self):
+        return self.next
+
+    def set_next(self, node):
+        self.next = node
+
+#implementation of linked list
+class linked_list(object):
+    def __init__(self, head = None):
+        self.head = head
+
+    def insert(self, data):
+        node = list_node(data)
+        node.set_next(self.head)
+        self.head = node
+
+    def get_size(self):
+        curr = self.head
+        count = 0
+        while curr:
+            count += 1
+            curr = curr.get_next()
+        return count
+
+    def search(self, data):
+        curr = self.head
+        while curr:
+            if curr.get_data() == data:
+                break
+            curr = curr.get_next()
+        return curr
+
+    def delete(self, data):
+        curr = self.head
+        prev = None
+        while curr:
+            if curr.get_data() == data:
+                break
+            prev = curr
+            curr = curr.get_next()
+        if curr is not None:
+            prev.next = curr.get_next()
+
+
 
 
