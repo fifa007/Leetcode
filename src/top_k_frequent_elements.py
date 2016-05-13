d664f0f0eb9ab0e85e6a7dbe23d408ffb63e0fa9
Hao Chen <hao.chen@avalara.com>
2016-05-13 16:55:46 -0700

    top k elements

---
+++ src/top_k_frequent_elements.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python2.7
+
+
+'''
+Given a non-empty array of integers, return the k most frequent elements.
+
+For example,
+Given [1,1,1,2,2,3] and k = 2, return [1,2].
+
+Note:
+You may assume k is always valid, 1 ≤ k ≤ number of unique elements.
+Your algorithm's time complexity must be better than O(n log n), where n is the array's size.
+'''
+
+import collections
+import heapq
+
+class Solution(object):
+    def get_top_k_elem(self, nums, k):
+        c = collections.Counter(nums)
+        return heapq.nlargest(k, c, c.get())
