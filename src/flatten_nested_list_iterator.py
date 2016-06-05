af9a04b00b83f20fea4202862b85be06b3265230
Hao Chen <hao.chen@avalara.com>
2016-06-04 22:57:41 -0700

    flatten nested list iterator

---
+++ src/flatten_nested_list_iterator.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python2.7
+
+
+'''
+Given a nested list of integers, implement an iterator to flatten it.
+
+Each element is either an integer, or a list -- whose elements may also be integers or other lists.
+
+Example 1:
+Given the list [[1,1],2,[1,1]],
+
+By calling next repeatedly until hasNext returns false, the order of elements returned by next should be: [1,1,2,1,1].
+
+Example 2:
+Given the list [1,[4,[6]]],
+
+By calling next repeatedly until hasNext returns false, the order of elements returned by next should be: [1,4,6].
+'''
+
+# """
+# This is the interface that allows for creating nested lists.
+# You should not implement it, or speculate about its implementation
+# """
+#class NestedInteger(object):
+#    def isInteger(self):
+#        """
+#        @return True if this NestedInteger holds a single integer, rather than a nested list.
+#        :rtype bool
+#        """
+#
+#    def getInteger(self):
+#        """
+#        @return the single integer that this NestedInteger holds, if it holds a single integer
+#        Return None if this NestedInteger holds a nested list
+#        :rtype int
+#        """
+#
+#    def getList(self):
+#        """
+#        @return the nested list that this NestedInteger holds, if it holds a nested list
+#        Return None if this NestedInteger holds a single integer
+#        :rtype List[NestedInteger]
+#        """
+
+# Your NestedIterator object will be instantiated and called as such:
+# i, v = NestedIterator(nestedList), []
+# while i.hasNext(): v.append(i.next())
+
+
+
+class NestedIterator(object):
+    def helper(self, nested_list, idx, ret):
+        if idx == len(nested_list):
+            return
+        if nested_list[idx].isInteger():
+            ret.append(nested_list[idx].getInteger())
+        else:
+            self.helper(nested_list[idx].getList(), 0, ret)
+        self.helper(nested_list, idx+1, ret)
+
+    def __init__(self, nestedList):
+        self.elems = []
+        self.index = 0
+        self.helper(nestedList, 0, self.elems)
+
+    def has_next(self):
+        return self.index < len(self.elems)
+
+    def next(self):
+        val = self.elems[self.index]
+        self.indx += 1
+        return val
+
